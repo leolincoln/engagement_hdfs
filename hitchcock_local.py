6ea70ba2f4eedce59dd84852138e3f1c69fcc7a2
leolincoln <leoliu@u.northwestern.edu>
2016-02-03 00:09:44 -0600

    remove process

--- hitchcock_local.py
+++ hitchcock_local.py
@@ -151,7 +151,7 @@ def main():
     #result_dict = manager.dict()
     for subject in xrange(len(data)):
         #newfileName = fileName[:-4]+str(subject)+'.dat'
-        p = Process(target=subject_worker, args=(f,data,subject,result_dict))
+        #p = Process(target=subject_worker, args=(f,data,subject,result_dict))
         subject_worker(f,data,subject,result_dict)
         #processes.append(p)
         #p.start()
