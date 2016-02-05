3c11fa417e2279ca9712da792f61f2a13955dce8
leolincoln <leoliu@u.northwestern.edu>
2016-02-04 19:20:38 -0600

    changed kmeans specifications

--- runYarn.bash
+++ runYarn.bash
@@ -14,7 +14,7 @@ export HADOOP_CONF_DIR=/etc/alternatives/hadoop-conf
 /opt/cloudera/parcels/CDH/bin/spark-submit \
 --master yarn \
 --deploy-mode client \
-#--num-executors 2 \
-#--executor-cores 4 \
-#--executor-memory 15g \
+--num-executors 4 \
+--executor-cores 8 \
+--executor-memory 7g \
 $1 $2 $3
